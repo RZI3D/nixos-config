08a6910c896b7fdbbbf7b86ece7982b0b37a5eb6
RZI3D <71341522+RZI3D@users.noreply.github.com>
2026-03-04 22:25:07 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -9,16 +9,23 @@
     caelestia-shell = {
       url = "github:caelestia-dots/shell";
       inputs.nixpkgs.follows = "nixpkgs";
+    vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.config.allowUnfree = true;
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, caelestia-shell, ... }: {
+  outputs = { self, nixpkgs, home-manager, caelestia-shell, ... }@inputs: {
     nixosConfigurations.caelestia = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
+    specialArgs = { inherit inputs; };
     modules = [
       ./hosts/e14-nix/default.nix
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; }; 
           home-manager.users.zackariyyasattaur = {
             home.username = "zackariyyasattaur";
             home.homeDirectory = "/home/zackariyyasattaur";
