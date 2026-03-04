937c7ec75f59b618ec1c10d1e168c6010a7dbbc7
RZI3D <71341522+RZI3D@users.noreply.github.com>
2026-03-04 19:39:53 +0300

    Update flake.nix
    Add caelestia

--- flake.nix
+++ flake.nix
@@ -6,30 +6,16 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # The magic ingredient: end4's logic turned into a Nix module
-    illogical-flake = {
-      url = "github:soymou/illogical-flake";
+    caelestia-shell = {
+      url = "github:caelestia-dots/shell";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, home-manager, illogical-flake, ... }: {
-    nixosConfigurations.end4-config = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager, caelestia-shell, ... }: {
+    nixosConfigurations.caelestia = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
       ./hosts/e14-nix/default.nix
-      ({ pkgs, ... }: {
-        nixpkgs.overlays = [
-          (final: prev: {
-            pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-              (python-final: python-prev: {
-                kde-material-you-colors = python-final.callPackage ./pkgs/kde-material-you-colors/default.nix { };
-              })
-            ];
-          })
-        ];
-        })
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
@@ -38,8 +24,7 @@
             home.homeDirectory = "/home/zackariyyasattaur";
             home.stateVersion = "24.11";
             imports = [
-              illogical-flake.homeManagerModules.default
-              ./modules/desktop/illogical-impulse.nix
+              ./modules/desktop/caelestia.nix
             ];
           };
         }
