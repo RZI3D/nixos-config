e39498452c5da26f5dd6beac8932c60e91b52687
RZI3D <71341522+RZI3D@users.noreply.github.com>
2026-03-04 15:59:59 -0500

    Add missing line

--- flake.nix
+++ flake.nix
@@ -47,6 +47,7 @@
       system = "x86_64-linux";
       modules = [
       ./hosts/e14-nix/default.nix
+        home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.extraSpecialArgs = { inherit inputs; }; 
