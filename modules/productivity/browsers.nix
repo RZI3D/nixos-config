d49fa2cb95acc5d113c5f7bb1271d701ace98466
RZI3D <71341522+RZI3D@users.noreply.github.com>
2026-03-05 04:53:03 +0300

    Update browsers.nix

--- modules/productivity/browsers.nix
+++ modules/productivity/browsers.nix
@@ -5,39 +5,28 @@
     enable = true;
     
     # This applies settings to ALL profiles
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = false; # gotta figure out what this does 
-      EnableTrackingProtection = {
-        Value = true;
-        Locked = true;
-        Cryptomining = true;
-        Fingerprinting = true;
-      };
-
-      # Force installing extensions by ID
-      ExtensionSettings = {
-        "*".installation_mode = "allowed"; # Allow manual installs too if you want
-
-        # uBlock Origin
-        "uBlock0@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
+    programs.firefox.policies = {
+      ExtensionSettings = with builtins;
+        let extension = shortId: uuid: {
+          name = uuid;
+          value = {
+            install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+            installation_mode = "normal_installed";
+          };
         };
-
-        # Bitwarden
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-          installation_mode = "force_installed";
-        };
-
-        # Dark Reader
-        "addon@darkreader.org" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-          installation_mode = "force_installed";
-        };
-      };
+        in listToAttrs [
+          (extension "ublock-origin" "uBlock0@raymondhill.net")
+          (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+          (extension "clearurls" "{74145f27-f039-47ce-a470-a662b129930a}")
+          # TODO: Get rest of extensions later
+        ];
+        # To add additional extensions, find it on addons.mozilla.org, find
+        # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+        # Then, download the XPI by filling it in to the install_url template, unzip it,
+        # run `jq .browser_specific_settings.gecko.id manifest.json` or
+        # `jq .applications.gecko.id manifest.json` to get the UUID
     };
+  };
 
     profiles.zackariyya = {
       isDefault = true;
