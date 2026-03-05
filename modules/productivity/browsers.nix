d259b18c74daafab33451e6348dd1cef3f86d580
RZI3D <71341522+RZI3D@users.noreply.github.com>
2026-03-05 04:41:49 +0300

    Update browsers.nix

--- modules/productivity/browsers.nix
+++ modules/productivity/browsers.nix
@@ -1,28 +1,48 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 
-let
-  # This grabs the addon packages for your specific system (x86_64-linux)
-  addons = inputs.firefox-addons.packages.${pkgs.system};
-in
 {
   programs.firefox = {
     enable = true;
+    
+    # This applies settings to ALL profiles
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = false; # gotta figure out what this does 
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+
+      # Force installing extensions by ID
+      ExtensionSettings = {
+        "*".installation_mode = "allowed"; # Allow manual installs too if you want
+
+        # uBlock Origin
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+
+        # Bitwarden
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
+
+        # Dark Reader
+        "addon@darkreader.org" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
+
     profiles.zackariyya = {
       isDefault = true;
-      
-      extensions = with addons; [
-        ublock-origin
-        bitwarden
-        darkreader
-        catppuccin-gh-flavors
-        sponsorblock
-        return-youtube-dislike
-      ];
-
       settings = {
-        "browser.formfill.enable" = false;          # Don't save addresses/phones
-        "privacy.trackingprotection.enabled" = true;
-        "signon.rememberSignons" = false;           # Use Bitwarden instead!
+        # may be useful later
       };
     };
   };
